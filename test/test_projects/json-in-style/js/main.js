3becb97189ff4e215291f8848fb05693978e86ac
Christopher Peisert <cpeisert@gmail.com>
2012-05-23 18:56:45 -0700

    json-in-style: app layout changed to two columns with style select buttons on right

--- test/test_projects/json-in-style/js/main.js
+++ test/test_projects/json-in-style/js/main.js
@@ -14,28 +14,36 @@
  * under the License.
  */
 
-goog.provide('test.jsonstyle');
+goog.provide('jsonstyle');
 
 goog.require('goog.dom');
 goog.require('goog.events');
 goog.require('goog.events.Event');
 goog.require('goog.events.EventType');
 goog.require('goog.format.JsonPrettyPrinter');
+goog.require('goog.ui.Popup');
 
 
 /** @type {string} */
-test.jsonstyle.outputElementID = "outputPrettyPrint";
+jsonstyle.colorPopupID = "colorPopup";
 
 /** @type {string} */
-test.jsonstyle.inputTextareaID = "jsonInputTextarea";
+jsonstyle.outputElementID = "outputPrettyPrint";
+
+/** @type {string} */
+jsonstyle.inputTextareaID = "jsonInputTextarea";
 
 /** @type {!Element} */
-test.jsonstyle.outputElement = /** @type {!Element} */
-    (document.getElementById(test.jsonstyle.outputElementID));
+jsonstyle.colorPopupElement = /** @type {!Element} */
+    (document.getElementById(jsonstyle.colorPopupID));
 
 /** @type {!Element} */
-test.jsonstyle.inputTextarea = /** @type {!Element} */
-    (document.getElementById(test.jsonstyle.inputTextareaID));
+jsonstyle.outputElement = /** @type {!Element} */
+    (document.getElementById(jsonstyle.outputElementID));
+
+/** @type {!Element} */
+jsonstyle.inputTextarea = /** @type {!Element} */
+    (document.getElementById(jsonstyle.inputTextareaID));
 
 
 /**
@@ -46,7 +54,7 @@ test.jsonstyle.inputTextarea = /** @type {!Element} */
  * @param {number=} indentation Number of spaces to indent each level. Defaults
  *     to 2.
  */
-test.jsonstyle.prettyPrintJSON = function(json, element, indentation) {
+jsonstyle.prettyPrintJSON = function(json, element, indentation) {
   var delimiters = new goog.format.JsonPrettyPrinter.HtmlDelimiters();
   delimiters.indent = indentation || 2;
   var formatter = new goog.format.JsonPrettyPrinter(delimiters);
@@ -59,16 +67,80 @@ test.jsonstyle.prettyPrintJSON = function(json, element, indentation) {
  *
  * @param {!goog.events.Event} event The change event.
  */
-test.jsonstyle.inputTextareaListener = function(event) {
+jsonstyle.inputTextareaListener = function(event) {
   try {
-    test.jsonstyle.prettyPrintJSON(test.jsonstyle.inputTextarea.value,
-        test.jsonstyle.outputElement);
+    jsonstyle.prettyPrintJSON(jsonstyle.inputTextarea.value,
+        jsonstyle.outputElement);
   } catch (error) {
-    test.jsonstyle.outputElement.innerHTML =
+    jsonstyle.outputElement.innerHTML =
         '<span class="error">JSON syntax error.</span>';
   }
-}
+};
 
-goog.events.listen(test.jsonstyle.inputTextarea, goog.events.EventType.CHANGE,
-    test.jsonstyle.inputTextareaListener);
+/*
+ * Event handlers for the color popup.
+ */
+
+/** @type {goog.ui.Popup} */
+jsonstyle.colorPopup = new goog.ui.Popup(
+    jsonstyle.colorPopupElement);
+
+/**
+ * Make the color-selector popup window visible.
+ */
+jsonstyle.showPopup = function() {
+  /*var popup = jsonstyle.colorPopup;
+
+  var btn = document.getElementById('btn');
+  var buttonCorner = toCorner(
+      getCheckedValue(document.forms[0].elements['button_corner']));
+  var menuCorner = toCorner(
+      getCheckedValue(document.forms[0].elements['menu_corner']));
+
+  var t = parseInt(document.getElementById('margin_top').value);
+  var r = parseInt(document.getElementById('margin_right').value);
+  var b = parseInt(document.getElementById('margin_bottom').value);
+  var l = parseInt(document.getElementById('margin_left').value);
+  var margin = new goog.math.Box(t, r, b, l);
+
+  popup.setVisible(false);
+  popup.setPinnedCorner(menuCorner);
+  popup.setMargin(margin);
+  popup.setPosition(new goog.positioning.AnchoredViewportPosition(btn,
+      buttonCorner));
+  popup.setVisible(true);*/
+};
+
+/**
+ * Initialize the popup used to display the HSV color selector.
+ */
+jsonstyle.initializeColorPopup = function() {
+  jsonstyle.colorPopup.setHideOnEscape(true);
+  jsonstyle.colorPopup.setAutoHide(true);
+
+  //goog.events.listen(window, goog.events.EventType.RESIZE, onResize);
+};
+
+/**
+ * Initialize JSON-In-Style application.
+ */
+jsonstyle.initializeApp = function() {
+  document.getElementById('buttonPropertyName').className = 
+      goog.getCssName('.goog-jsonprettyprinter-propertyname');
+  document.getElementById('buttonBooleanValue').className =
+      goog.getCssName('.goog-jsonprettyprinter-propertyvalue-boolean');
+  document.getElementById('buttonNullValue').className =
+      goog.getCssName('.goog-jsonprettyprinter-propertyvalue-null');
+  document.getElementById('buttonNumberValue').className =
+      goog.getCssName('.goog-jsonprettyprinter-propertyvalue-number');
+  document.getElementById('buttonStringValue').className =
+      goog.getCssName('.goog-jsonprettyprinter-propertyvalue-string');
+
+  jsonstyle.initializeColorPopup();
+  goog.events.listen(jsonstyle.inputTextarea, goog.events.EventType.CHANGE,
+      jsonstyle.inputTextareaListener);
+};
+
+// Application entry point.
+jsonstyle.initializeApp();
 
