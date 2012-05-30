b7a6cabcf39939859e740e9cfa5103cc19222a37
Christopher Peisert <cpeisert@gmail.com>
2012-05-29 22:28:09 -0700

    Remove Ant property references to Closure Stylesheet locations since Stylesheets will now be included internally within the closure-ant-tasks jar

--- src/org/closureextensions/ant/SharedAntProperty.java
+++ src/org/closureextensions/ant/SharedAntProperty.java
@@ -34,10 +34,7 @@ public enum SharedAntProperty {
   CLOSURE_LIBRARY_DIR("closure-library.dir"),
   /** The directory where the Closure Builder python scripts are located. */
   CLOSURE_BUILDER_DIR("closure-builder.dir"),
-  /**
-   * The file path to closurebuilder.py, which is the entry point for Closure
-   * Builder.
-   */
+  /** The file path to closurebuilder.py. */
   CLOSURE_BUILDER_PY("closurebuilder.py"),
   /** The file path to depswriter.py. */
   DEPS_WRITER_PY("depswriter.py"),
@@ -47,23 +44,6 @@ public enum SharedAntProperty {
   CLOSURE_COMPILER_DIR("closure-compiler.dir"),
   /** The file path to the Closure Compiler jar file. */
   CLOSURE_COMPILER_JAR("closure-compiler.jar"),
-  /** The directory where Closure Stylesheets is located. */
-  CLOSURE_STYLESHEETS_DIR("closure-stylesheets.dir"),
-  /** The file path to the Closure Stylesheets jar file. */
-  CLOSURE_STYLESHEETS_JAR("closure-stylesheets.jar"),
-  /** The directory where Closure Templates is located. */
-  CLOSURE_TEMPLATES_DIR("closure-templates.dir"),
-  /** The file path to the Closure Templates jar file. */
-  CLOSURE_TEMPLATES_JAR("closure-templates.jar"),
-  /**
-   * The file path to soyutils.js, which is a component of Closure Templates.
-   */
-  SOYUTILS_JS("soyutils.js"),
-  /**
-   * The file path to soyutils_usegoog.js, which is a component of Closure
-   * Templates.
-   */
-  SOYUTILS_USEGOOG_JS("soyutils_usegoog.js"),
   /** The directory where plovr is located. */
   PLOVR_DIR("plovr.dir"),
   /** The file path to the plovr jar file. */
