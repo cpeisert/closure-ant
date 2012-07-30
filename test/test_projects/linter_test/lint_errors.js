6c54f4074fee8011cb40c30a39f2dd7b2d939df4
Christopher Peisert <cpeisert@gmail.com>
2012-07-29 23:49:56 -0700

    linter_test: new test project to test Closure Linter

---
+++ test/test_projects/linter_test/lint_errors.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2012 Christopher Peisert. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+goog.provide('lint.errors');
+goog.require('goog.dom');
+/**
+ * enough lint errors to destroy a dryer
+ *
+ * @param {string} param1 description is not a sentence
+ * @param param2 missing type annotation
+ */
+lint.errors.MyConstructor = function(param1) {
+  var x = goog.dom.getElementsByTagNameAndClass("body");
+  var longString = 'This is a long string and the concatenation operator '
+      + 'should appear on the previous line.';
+}
